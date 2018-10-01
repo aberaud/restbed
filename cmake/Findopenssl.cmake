d3d35267504d95be16711e6c71987d3f074a774b
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2018-10-01 16:39:44 -0400

    build: find openssl from alternative path

--- cmake/Findopenssl.cmake
+++ cmake/Findopenssl.cmake
@@ -6,6 +6,24 @@ find_library( crypto_LIBRARY_SHARED libcrypto.so libcrypto.dylib libeay32.dll HI
 
 find_path( ssl_INCLUDE openssl/ssl.h HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/inc32" "${PROJECT_SOURCE_DIR}/dependency/openssl/include" "/usr/local/opt/openssl/include" "/usr/include" "/usr/local/include" "/opt/local/include" )
 
+if ( NOT ssl_LIBRARY_STATIC )
+  find_library( ssl_LIBRARY_STATIC libssl.a ssleay32 HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" NO_CMAKE_FIND_ROOT_PATH)
+endif ( )
+if ( NOT crypto_LIBRARY_STATIC )
+  find_library( crypto_LIBRARY_STATIC libcrypto.a libeay32 HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" NO_CMAKE_FIND_ROOT_PATH)
+endif ( )
+
+if ( NOT ssl_LIBRARY_SHARED )
+  find_library( ssl_LIBRARY_SHARED libssl.so libssl.dylib ssleay32.dll HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" NO_CMAKE_FIND_ROOT_PATH)
+endif ( )
+if ( NOT crypto_LIBRARY_SHARED )
+  find_library( crypto_LIBRARY_SHARED libcrypto.so libcrypto.dylib libeay32.dll HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" NO_CMAKE_FIND_ROOT_PATH)
+endif ( )
+
+if ( NOT ssl_INCLUDE )
+  find_path( ssl_INCLUDE openssl/ssl.h HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/inc32" "${PROJECT_SOURCE_DIR}/dependency/openssl/include" "/usr/local/opt/openssl/include" "/usr/include" "/usr/local/include" "/opt/local/include" NO_CMAKE_FIND_ROOT_PATH)
+endif ( )
+
 if ( ssl_LIBRARY_STATIC AND ssl_LIBRARY_SHARED AND crypto_LIBRARY_STATIC AND crypto_LIBRARY_SHARED )
     set( OPENSSL_FOUND TRUE )
     add_definitions( -DBUILD_SSL=TRUE )
