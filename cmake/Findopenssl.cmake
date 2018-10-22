a26b364e03228de27498974bb1bbebd73145d30e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2018-10-21 20:30:53 -0400

    build: allow only static or shared crypto lib

--- cmake/Findopenssl.cmake
+++ cmake/Findopenssl.cmake
@@ -24,7 +24,7 @@ if ( NOT ssl_INCLUDE )
   find_path( ssl_INCLUDE openssl/ssl.h HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/inc32" "${PROJECT_SOURCE_DIR}/dependency/openssl/include" "/usr/local/opt/openssl/include" "/usr/include" "/usr/local/include" "/opt/local/include" NO_CMAKE_FIND_ROOT_PATH)
 endif ( )
 
-if ( ssl_LIBRARY_STATIC AND ssl_LIBRARY_SHARED AND crypto_LIBRARY_STATIC AND crypto_LIBRARY_SHARED )
+if ( (ssl_LIBRARY_STATIC AND crypto_LIBRARY_STATIC) OR (ssl_LIBRARY_SHARED AND crypto_LIBRARY_SHARED) )
     set( OPENSSL_FOUND TRUE )
     add_definitions( -DBUILD_SSL=TRUE )
 
